c7c4a0a8ad9209cf5f7bbc63b24243107c8844eb
Sidharth-123 <sidharthpradhan9938@gmail.com>
2021-05-28 18:31:15 +0530

    First Commit

---
+++ src/main/java/com/test/hello_world/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package com.test.hello_world;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ApplicationConfiguration {
+@RequestMapping("/hello")
+public String hello() {
+	return "Hello World!";
+}
+}
